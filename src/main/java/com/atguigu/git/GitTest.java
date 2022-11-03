55c72124ff05091333e58b3809cdbf2df1fef0f7
RussYyj0 <117342990+RussYyj0@users.noreply.github.com>
2022-11-03 16:07:38 +0800

    pull committttt

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("pushtest");
         System.out.println("pushtest");
         System.out.println("pulltest");
+        System.out.println("pulltest44444444444444");
     }
 }
