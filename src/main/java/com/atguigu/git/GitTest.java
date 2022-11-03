6def3535ca0640f64b3fdfc5f5a045bb110caf61
Layne <Layne@atguigu.com>
2022-11-03 15:51:12 +0800

    posh commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("hello git4");
         System.out.println("hello git444444444444");
+        System.out.println("pushtest");
     }
 }
